4dc4f71ca9251b403579329ca1c4b1e08ea36131
Acris Liu <acrisliu@gmail.com>
2020-01-08 14:29:33 +0800

    Fix lightgallery

--- source/js/lightgallery.js
+++ source/js/lightgallery.js
@@ -1,3 +1,14 @@
 function initLightGallery() {
+    document.querySelectorAll('.post-content :not(a) > img').forEach(image => {
+        let imageSrc = image.getAttribute('src');
+        let imageWrapLink = document.createElement('a');
+        imageWrapLink.setAttribute("href", imageSrc);
+        image.parentNode.insertBefore(imageWrapLink, image);
+        let imageTitle = image.getAttribute('title') || image.getAttribute('alt');
+        if (imageTitle) {
+            imageWrapLink.setAttribute('title', imageTitle);
+        }
+    });
+
     lightGallery(document.getElementsByClassName('post-content'));
 }
